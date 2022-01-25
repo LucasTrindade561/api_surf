67ac6f634cd8fd46473ede17568d93d4fa26fc77
Lucas Trindade <nh.lucass@gmail.com>
2022-01-25 20:48:16 -0300

    routes athletes

--- src/routes/athletesRoutes.js
+++ src/routes/athletesRoutes.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import AthleteController from '../controllers/AthleteController';
-// import loginRequired from '../middlewares/loginRequired';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
 // athletes
 router.get('/', AthleteController.index);
-router.post('/', AthleteController.store);
+router.post('/', loginRequired, AthleteController.store);
 router.get('/:id', AthleteController.show);
-router.put('/:id', AthleteController.update);
-router.delete('/:id', AthleteController.delete);
+router.put('/:id', loginRequired, AthleteController.update);
+router.delete('/:id', loginRequired, AthleteController.delete);
 
 export default router;
